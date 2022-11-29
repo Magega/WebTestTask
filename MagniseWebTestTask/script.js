1ae61d78106c811c9afef072b002d2c101f35b98
Nazar Magega <47562065+Magega@users.noreply.github.com>
2022-11-29 05:22:56 +0200

    Add files via upload

---
+++ MagniseWebTestTask/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySviper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+  });
